ac5c03644de967339ceb539618d56c884e122ad4
Marcobc-exe <marco.developing@gmail.com>
2026-03-27 10:57:48 -0600

    Restructure project into api and client folders

--- app/database.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_db_connection():
-  return psycopg2.connect(
-    host="localhost",
-    database="password_manager",
-    user="postgres",
-    password="$bonfire$123"
-  )
