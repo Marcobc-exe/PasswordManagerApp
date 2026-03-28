20543bd8e74812e6f3b3a1d55150761992f2c658
Marcobc-exe <marco.developing@gmail.com>
2026-03-28 11:04:20 -0600

    style: 2 columns password

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -150,24 +150,35 @@ export default function Dashboard() {
         </button>
       </div>
 
-      <div className="flex flex-col items-center w-full gap-4">
+      <div
+        className="
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          gap-4
+          w-full
+          max-w-4xl
+          mx-auto
+          place-items-center
+        "
+      >
         {passwords.map((p: PasswordsProps) => {
           const isVisible = visiblePasswords.includes(p.id);
 
           return (
             <div
               key={p.website}
-              className="w-full max-w-md bg-zinc-900 p-5 rounded-2xl mb-4 flex justify-between items-center hover:bg-zinc-800 transition"
+              className="w-full max-w-md bg-zinc-900 p-5 rounded-2xl flex justify-between items-center hover:bg-zinc-800 transition"
             >
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-1 w-2/3">
                 <p className="text-lg font-semibold">{p.website}</p>
                 <p className="text-zinc-400 text-sm">{p.username}</p>
-                <p className="font-mono mt-2 text-lg">
+                <p className="font-mono mt-2 text-lg truncate">
                   {isVisible ? p.password : "*********"}
                 </p>
               </div>
 
-              <div className="flex gap-3">
+              <div className="flex gap-2">
                 <button
                   onClick={() => togglePassword(p.id)}
                   className="bg-zinc-800 p-2 rounded-lg hover:bg-zinc-700 transition"
