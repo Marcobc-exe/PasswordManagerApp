c61eb0171897cc148b5062566490a5bd9fabf148
Marcobc-exe <marco.developing@gmail.com>
2026-03-29 10:35:35 -0600

    feat: logout btn + search bar + dark/light theme in progress

--- client/app/dashboard/ErrorMessage.tsx
+++ client/app/dashboard/ErrorMessage.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { FC } from "react";
 
 type Props = {
   error: string;
 };
 
-export const Error: FC<Props> = ({ error }) => {
+export const ErrorMsg: FC<Props> = ({ error }) => {
   return (
     <main className="min-h-screen bg-black text-white flex items-center justify-center">
       <p className="text-red-500 text-xl">{error}</p>
--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -7,8 +7,8 @@ import { AddPassModal } from "./addPassModal";
 import { PasswordsProps } from "../type";
 import { PasswordCards } from "./passwordCards";
 import { NoPasswordsYet } from "./noPasswords";
-import { Error } from "./error";
 import { Loading } from "./Loading";
+import { ErrorMsg } from "./ErrorMessage";
 
 export default function Dashboard() {
   const [passwords, setPasswords] = useState<PasswordsProps[]>([]);
@@ -19,6 +19,8 @@ export default function Dashboard() {
   const [website, setWebsite] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [search, setSearch] = useState("");
+  const [darkMode, setDarkMode] = useState(true);
   const isMobile = useMediaQuery("(max-width: 640px)");
 
   useEffect(() => {
@@ -105,31 +107,66 @@ export default function Dashboard() {
     window.location.reload();
   };
 
-  if (loading) return <Loading />
-  if (error) return <Error error={error}  />
-  if (passwords.length == 0) return <NoPasswordsYet />
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
+
+  if (loading) return <Loading />;
+  if (error) return <ErrorMsg error={error} />;
+  if (passwords.length == 0) return <NoPasswordsYet />;
 
   return (
-    <main className="min-h-screen bg-black text-white p-10">
+    <main
+      className={`min-h-screen p-10 transition-all duration-700 ${
+        darkMode
+          ? "bg-linear-to-br from-indigo-950 via-zinc-900 to-black text-white"
+          : "bg-linear-to-br from-[tan] from-1% via-white via-90% to-[#f3d5ae]"
+      }`}
+    >
       <div className="flex justify-between items-center mb-8">
-        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <h1 className="text-3xl font-bold text-white">Dashboard</h1>
 
-        <button
-          className={`flex items-center gap-2 bg-blue-600 ${isMobile ? "p-3 rounded-3xl" : "px-5 py-3 rounded-xl"}  hover:bg-blue-700 transition cursor-pointer`}
-          onClick={() => setOpenModal(true)}
-        >
-          <Plus size={isMobile ? 24 : 18} />
-          {!isMobile ? "Add password" : ""}
-        </button>
+        <div className="flex justify-between items-end gap-3">
+          <button
+            className={`flex items-center gap-2 bg-blue-600 ${isMobile ? "p-3 rounded-3xl" : "px-5 py-3 rounded-xl"}  hover:bg-blue-700 transition cursor-pointer`}
+            onClick={() => setOpenModal(true)}
+          >
+            <Plus size={isMobile ? 24 : 18} />
+            {!isMobile ? "Add password" : ""}
+          </button>
+          <button
+            onClick={() => setDarkMode(!darkMode)}
+            className="bg-zinc-700 cursor-pointer px-5 py-3 rounded-lg hover:bg-zinc-800 transition"
+          >
+            {darkMode ? "Light Mode" : "Dark Mode"}
+          </button>
+          <button
+            onClick={handleLogout}
+            className="bg-red-600 cursor-pointer px-5 py-3 rounded-xl hover:bg-red-700 transition"
+          >
+            Logout
+          </button>
+        </div>
       </div>
 
-      <PasswordCards
-        passwords={passwords}
-        visiblePasswords={visiblePasswords}
-        handleTogglePassword={togglePassword}
-        handleCopyPasswords={copyPasswords}
-        handleDeletePassword={deletePassword}
-      />
+      <div className="max-w-4xl mx-auto">
+        <input
+          type="text"
+          placeholder="Search passwords..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-full mb-8 bg-zinc-900/70 backdrop-blur px-5 py-3 rounded-2xl outline-none border border-zinc-800 focus:border-zinc-500 transition"
+        />
+        <PasswordCards
+          search={search}
+          passwords={passwords}
+          visiblePasswords={visiblePasswords}
+          handleTogglePassword={togglePassword}
+          handleCopyPasswords={copyPasswords}
+          handleDeletePassword={deletePassword}
+        />
+      </div>
       <AddPassModal
         openModal={openModal}
         website={website}
